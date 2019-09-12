8b14c4836d04523a7c5c3accd65289218a4da579
Arthur <arthur.ext@gmail.com>
2019-09-12 15:19:33 -0300

    create connect function

---
+++ database/conn.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+from urllib.parse import quote_plus
+
+def conn():    
+    user = "root"
+    pswd = "root"
+    host = "localhost:27017"
+    uri = "mongodb://%s:%s@%s" % (
+        quote_plus(user),
+        quote_plus(pswd),
+        host    
+    )
+
+    client = MongoClient(uri)
+    base = client.parser_quake
+
+    return base
