a9fe363302440d7385423b55110fb882472a54c5
Arthur <arthur.ext@gmail.com>
2019-09-12 03:23:27 -0300

    add games.log

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print(__name__)
+import argparse
+
+if __name__ == "__main__":
+    arguments = argparse.ArgumentParser(description="Parse Quake Log File")
+    
+    arguments.add_argument("-l", "--logfile",
+                            required=True,
+                            dest='logfile',
+                            metavar='log file',
+                            help="log file path")
+    
+    args = arguments.parse_args()
+    print(args.logfile)
+    
